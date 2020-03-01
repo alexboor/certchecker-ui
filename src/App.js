5193fdeb5f2cf675f4653eb1ce3ac6514b56fcbc
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2020-03-01 21:28:35 +0100

    Here we go

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Menubar} from 'primereact/menubar';
+import {Button} from 'primereact/button';
+import {DataTable} from 'primereact/datatable';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app">
+      <div className="menu">
+        <a>
+            <p className="menu-title">
+                <i className="pi pi-globe" style={{'position':'relative','top': '3px'}}></i>
+                HTTP Guard UI
+            </p>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'primereact/resources/themes/luna-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+require('classnames');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
