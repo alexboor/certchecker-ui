a1ca9e36a3c25b509373df4e4362320cf609de9b
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2020-03-09 23:01:01 +0100

    Buttons toolbar on Domain page. 'Return' event impl.

--- src/Domain/Domain.js
+++ src/Domain/Domain.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import {navigate} from 'hookrouter';
 import DomainContext from "../DomainsContext";
+import {Button} from "primereact/button";
 
 /**
  * Domain component
  * @props id {numeric} Domain ID will be shown
  */
-
 export default class Domain extends Component {
     static contextType = DomainContext;
 
@@ -25,9 +26,35 @@ export default class Domain extends Component {
     render() {
         console.log(this.state);
 
-        return (<div>Domain {this.props.id}</div>)
+        return (
+            <div>
+                <Buttons />
+
+                { !!Object.keys(this.state.domain).length ? <DomainContainer domain={this.state.domain}/> : <Loader />}
+            </div>
+        )
     }
 }
+
+const Loader = props => <div style={{'margin':'10px 0 0 0'}}><i className="pi pi-spin pi-spinner"></i></div>
+
+const Buttons = (props) => {
+    return (
+        <>
+            <Button icon="pi pi-angle-left" onClick={e => navigate('/')}/>
+            <Button label="Remove" className="p-button-danger" style={{'marginLeft':'5px'}}/>
+        </>
+    )
+};
+
+const DomainContainer = props => {
+    console.log(props.domain)
+    return (
+        <div>
+            <h1>{props.domain.Name}</h1>
+        </div>
+    )
+};
 
 // const Domain = (props) => {
 //
